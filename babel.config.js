1ff30aebc936876bb5bfb396b14091832cddbd44
Maxim <anufriev.maxim29@gmail.com>
2025-07-28 19:56:28 +0300

    chore: configure path aliases and define core module paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "@api": "./src/api",
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@configs": "./src/config",
+          "@constants": "./src/constants",
+          "@helpers": "./src/helpers",
+          "@layouts": "./src/layouts",
+          "@routes": "./src/routes",
+          "@screens": "./src/screens",
+          "@store": "./src/store",
+          "@styles": "./src/styles",
+          "@types": "./srs/types",
+          "@utils": "./src/utils",
+        },
+      },
+    ],
+  ],
 };
