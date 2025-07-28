31cf80afd433cdb5362c6b057e50311a20a1e573
Maxim <anufriev.maxim29@gmail.com>
2025-07-28 20:41:54 +0300

    feat: added screens — Home, ProductDetail, Basket, Favorites, SignUp, SignIn, and AuthInit

---
+++ src/screens/Auth/AuthInitScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AuthInitScreen = () => {
+  return (
+    <View>
+      <Text>AuthInitScreen</Text>
+    </View>
+  )
+}
+
+export default AuthInitScreen
+
+const styles = StyleSheet.create({})
