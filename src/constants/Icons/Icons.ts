d73b5378362e994979e27dd2692485c434a7649e
Maxim <anufriev.maxim29@gmail.com>
2025-07-29 23:16:27 +0300

    feat: add SVG icons to project

---
+++ src/constants/Icons/Icons.ts
@@ -0,0 +1,22 @@
+import HomeIcon from "@assets/svg/home.svg"
+import HeartOutlineIcon from "@assets/svg/heartOutline.svg"
+import BasketIcon from "@assets/svg/basket.svg"
+import UserIcon from "@assets/svg/user.svg"
+import HeartFilledIcon from "@assets/svg/heartFilled.svg"
+import DeleteIcon from "@assets/svg/delete.svg"
+import SearchIcon from "@assets/svg/search.svg"
+import ArrowBackIcon from "@assets/svg/arrowBack.svg"
+import GridIcon from "@assets/svg/grid.svg"
+
+export {
+    HomeIcon,
+    HeartOutlineIcon,
+    BasketIcon,
+    UserIcon,
+    HeartFilledIcon,
+    DeleteIcon,
+    SearchIcon,
+    ArrowBackIcon,
+    GridIcon,
+}
+
