8daebb7399427b314ab4ed61cb0ff6edbb31d9ec
Maxim <anufriev.maxim29@gmail.com>
2025-08-07 22:48:40 +0300

    feat: add navigation logic to ProductCard component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,21 @@
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { typography } from '@styles/typography';
 import { blackImage } from '@mocks/testImages';
 import LikeButton from '@ui/LikeButton/LikeButton';
 import { styles } from './styles';
+import { SCREENS } from '@routes/navigations.types';
+import useAppNavigation from '@hooks/useAppNavigation';
 
 const ProductCard = () => {
+  const navigation = useAppNavigation()
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={()=>navigation.navigate(SCREENS.PRODUCT_DETAIL)}>
       <Image source={blackImage} style={styles.image} />
       <LikeButton style={styles.likeButton} />
       <Text style={typography.smallTitle}>Jacket Jeans</Text>
       <Text style={typography.smallTitleSecondary}>$45.9</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
